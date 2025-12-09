a99187b06d03ae725645b5d5bcaef2e86ecf58e8
praff1351 <prafullihoo.7777@gmail.com>
2025-12-10 03:39:57 +0530

    42: reverted changes

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -10,13 +10,13 @@ import { createAdminClient } from "@/lib/appwrite";
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  // const h = await headers();
-  // const origin = h.get("origin");
+  const h = await headers();
+  const origin = h.get("origin");
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth?prompt=consent`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up?prompt=consent`
+    `${origin}/oauth?prompt=consent`,
+    `${origin}/sign-up?prompt=consent`
   );
   return redirect(redirectUrl);
 }
@@ -24,13 +24,13 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
-  // const h = await headers();
-  // const origin = h.get("origin");
+  const h = await headers();
+  const origin = h.get("origin");
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth?prompt=consent`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up?prompt=consent`
+    `${origin}/oauth?prompt=consent`,
+    `${origin}/sign-up?prompt=consent`
   );
   return redirect(redirectUrl);
 }
